1500f1c333fbccf9869e9e2c99baaf78e3e3e245
Joel Reeves <jreeves@tonicdesign.com>
2015-12-29 19:05:22 -0500

    nested if statement

--- Chapter_03/Conditionals.playground/Contents.swift
+++ Chapter_03/Conditionals.playground/Contents.swift
@@ -6,7 +6,15 @@ var population: Int = 5422
 var message: String
 var hasPostOffice: Bool = true
 
-message = population < 10000 ? "\(population) is a small town!" : "\(population) is pretty big!"
+if population < 10000 {
+    message = "\(population) is a small town!"
+} else {
+    if population >= 10000 && population < 50000 {
+        message = "\(population) is a medium town"
+    } else {
+       message = "\(population) is pretty big!"
+    }
+}
 
 print(message)
 
