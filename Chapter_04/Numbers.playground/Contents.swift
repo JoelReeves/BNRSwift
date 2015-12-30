5b6f73553a1a7cc1fc206f50b28d734ca4bde30b
Joel Reeves <jreeves@tonicdesign.com>
2015-12-30 01:52:03 -0500

    int conversion and floating point numbers

--- Chapter_04/Numbers.playground/Contents.swift
+++ Chapter_04/Numbers.playground/Contents.swift
@@ -42,3 +42,25 @@ print("x has had 10 added to it and is now \(x)")
 let y: Int8 = 120
 let z = y &+ 10 // using overflow operator
 print("120 &+ 10 is \(z)")
+
+let a: Int16 = 200
+let b: Int8 = 50
+let c = a + Int16(b) // converting type to allow addition
+
+let d1 = 1.2
+let d2: Double = 1.1
+let f1: Float = 100.3
+
+print(10.0 + 11.4)
+print(11.0 / 3.0)
+print(12.4 % 5.0)
+
+if d1 == d2 {
+    print("d1 and d2 are the same")
+}
+
+print("d1 + 0.1 is \(d1 + 0.1)")
+
+if d1 + 0.1 == 1.2 {
+    print("d1 + 0.1 is equal to 1.2")
+}
