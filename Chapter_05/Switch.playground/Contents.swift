f96d0ac7e0af490f780ca777a43913f4be1a7a32
Joel Reeves <jreeves@tonicdesign.com>
2015-12-30 16:23:42 -0500

    using value binding to include the status code

--- Chapter_05/Switch.playground/Contents.swift
+++ Chapter_05/Switch.playground/Contents.swift
@@ -2,25 +2,27 @@
 
 import Cocoa
 
-var statusCode: Int = 404
+var statusCode: Int = 4041
 var errorString: String = "The request failed with the error: "
 
 switch statusCode {
 case 100, 101:
-    errorString += " Informational, 1xx."
+    errorString += " Informational, \(statusCode)."
     
 case 204:
     errorString += "Successful but no content, 204"
     
 case 300...307:
-    errorString += "Redirection, 3xx"
+    errorString += "Redirection, \(statusCode)"
     
 case 400...417:
-    errorString += "Client error, 4xx"
+    errorString += "Client error, \(statusCode)"
     
 case 500...505:
-    errorString += "Server Error, 5xx"
+    errorString += "Server Error, \(statusCode)"
     
 default:
-    errorString = "Unknown. Please review the request and try again."
+    errorString = "\(statusCode) is not a known error code."
 }
+
+print(errorString)
