0ccf1acba1038ef93552d03d24800c610b793f06
Joel Reeves <jreeves@tonicdesign.com>
2015-12-30 15:36:24 -0500

    switch statement with multiple values

--- Chapter_05/Switch.playground/Contents.swift
+++ Chapter_05/Switch.playground/Contents.swift
@@ -3,21 +3,13 @@
 import Cocoa
 
 var statusCode: Int = 404
-var errorString: String
+var errorString: String = "The request failed with the error:"
 
 switch statusCode {
-case 400:
-    errorString = "Bad Request"
-    
-case 401:
-    errorString = "Unauthorized"
-    
-case 403:
-    errorString = "Forbidden"
-    
-case 404:
-    errorString = "Not Found"
+case 400, 401, 403, 404:
+    errorString = "There was something wrong with the request."
+    fallthrough // control transfer statement. used to go to the next case directly below
     
 default:
-    errorString = "None"
+    errorString += " Please review the request and try again."
 }
