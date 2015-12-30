a5395a4eb5646e52ffed17a119e50b37126a7fd8
Joel Reeves <jreeves@tonicdesign.com>
2015-12-29 19:07:17 -0500

    refactor

--- Chapter_03/Conditionals.playground/Contents.swift
+++ Chapter_03/Conditionals.playground/Contents.swift
@@ -8,12 +8,10 @@ var hasPostOffice: Bool = true
 
 if population < 10000 {
     message = "\(population) is a small town!"
+} else if population >= 10000 && population < 50000 {
+    message = "\(population) is a medium town"
 } else {
-    if population >= 10000 && population < 50000 {
-        message = "\(population) is a medium town"
-    } else {
-       message = "\(population) is pretty big!"
-    }
+    message = "\(population) is pretty big!"
 }
 
 print(message)
