0e7575be516247c10fc9d89f6c9a475aa61cf7bb
Joel Reeves <jreeves@tonicdesign.com>
2016-01-15 16:35:30 -0500

    replacing i iterator with _ since it's not referred to in the loop body

--- Chapter_06/Loops.playground/Contents.swift
+++ Chapter_06/Loops.playground/Contents.swift
@@ -4,8 +4,8 @@ import Cocoa
 
 var myFirstInt: Int = 0
 
-for i in 1...5 {
+for _ in 1...5 {
     myFirstInt++
-    print("myFirstInt equals \(myFirstInt) at iteration \(i)")
+    print(myFirstInt)
 }
 
