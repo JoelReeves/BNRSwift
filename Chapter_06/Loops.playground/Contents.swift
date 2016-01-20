38f51c53c40666d588e0ba14afe88d6666f92537
Joel Reeves <jreeves@tonicdesign.com>
2016-01-20 10:02:38 -0500

    two example of counting by 2 from 0-100 where each loop is run 5 times

--- Chapter_06/Loops.playground/Contents.swift
+++ Chapter_06/Loops.playground/Contents.swift
@@ -2,57 +2,19 @@
 
 import Cocoa
 
-var myFirstInt: Int = 0
-
+// using for-in loop
 for _ in 1...5 {
-    myFirstInt++
-    print(myFirstInt)
-}
-
-for case let i in 1...100 where i % 3 == 0 {
-    print(i)
-}
-
-for var i = 1; i < 6; ++i {
-    ++myFirstInt
-    print(myFirstInt)
-}
-
-var i = 1
-while i < 6 {
-    ++myFirstInt
-    print(myFirstInt)
-    ++i
-}
-
-var shields = 5
-var blastOverHeating = false
-var blasterFireCount = 0;
-var spaceDemonsDestroyed = 0
-
-while shields > 0 {
-    if spaceDemonsDestroyed == 500 {
-        print("You beat the game!")
-        break
+    for var i = 0; i <= 100; i+=2 {
+        print(i)
     }
-    
-    if blastOverHeating {
-        print("Blasters are overheated! Cooldown initiated")
-        sleep(5)
-        print("Blasters ready to fire")
-        sleep(1)
-        blastOverHeating = false
-        blasterFireCount = 0
-    }
-    
-    if blasterFireCount > 100 {
-        blastOverHeating = true
-        continue
-    }
-    
-    print("Fire blasters!")
-    
-    ++blasterFireCount
-    ++spaceDemonsDestroyed
 }
+
+// using for-case loop
+for _ in 1...5 {
+    for case let i in 0...100 where i % 2 == 0 {
+        print(i)
+    }
+}
+
+
 
