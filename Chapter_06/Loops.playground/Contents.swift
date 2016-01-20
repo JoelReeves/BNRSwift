02bf9b2d5052a2aae770c0df825b0d5d08bfb073
Joel Reeves <jreeves@tonicdesign.com>
2016-01-20 09:42:50 -0500

    using control transfer statements

--- Chapter_06/Loops.playground/Contents.swift
+++ Chapter_06/Loops.playground/Contents.swift
@@ -25,3 +25,34 @@ while i < 6 {
     ++i
 }
 
+var shields = 5
+var blastOverHeating = false
+var blasterFireCount = 0;
+var spaceDemonsDestroyed = 0
+
+while shields > 0 {
+    if spaceDemonsDestroyed == 500 {
+        print("You beat the game!")
+        break
+    }
+    
+    if blastOverHeating {
+        print("Blasters are overheated! Cooldown initiated")
+        sleep(5)
+        print("Blasters ready to fire")
+        sleep(1)
+        blastOverHeating = false
+        blasterFireCount = 0
+    }
+    
+    if blasterFireCount > 100 {
+        blastOverHeating = true
+        continue
+    }
+    
+    print("Fire blasters!")
+    
+    ++blasterFireCount
+    ++spaceDemonsDestroyed
+}
+
