28408f449e95b3d5a7ebdf03f6efd3db0379db4c
Joel Reeves <jreeves@tonicdesign.com>
2016-01-20 10:27:48 -0500

    logging characters in a string

--- Chapter_07/Strings.playground/Contents.swift
+++ Chapter_07/Strings.playground/Contents.swift
@@ -4,3 +4,7 @@ let playGround = "Hello, playground"
 
 var mutablePlayGround = "Hello, mutable playground"
 mutablePlayGround += "!"
+
+for c: Character in mutablePlayGround.characters {
+    print("\(c)")
+}
