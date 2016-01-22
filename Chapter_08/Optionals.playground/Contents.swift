cee2f5457e449dcf29a230e7286e350f15b61726
Joel Reeves <jreeves@tonicdesign.com>
2016-01-22 09:36:00 -0500

    force unwrapping an optional when it is nil

--- Chapter_08/Optionals.playground/Contents.swift
+++ Chapter_08/Optionals.playground/Contents.swift
@@ -1,17 +1,4 @@
 import Cocoa
 
 var errorCodeString: String?
-var errorDescription: String?
-errorCodeString = "404"
-
-if let theError = errorCodeString, errorCodeInteger = Int(theError) where errorCodeInteger == 404 {
-    errorDescription = ("\(errorCodeInteger + 200): the requested resource was not found.")
-}
-
-var upCaseErrorDescription = errorDescription?.uppercaseString
-errorDescription
-
-upCaseErrorDescription?.appendContentsOf(" PLEASE TRY AGAIN")
-upCaseErrorDescription
-
-let description = errorDescription ?? "No Error"
+print(errorCodeString!)
