95995dd0b7adbc14244ab27893e4a4a1a1daa4b4
Joel Reeves <jreeves@tonicdesign.com>
2016-01-22 09:03:16 -0500

    optional binding and where clauses

--- Chapter_08/Optionals.playground/Contents.swift
+++ Chapter_08/Optionals.playground/Contents.swift
@@ -3,6 +3,6 @@ import Cocoa
 var errorCodeString: String?
 errorCodeString = "404"
 
-if let theError = errorCodeString, errorCodeInteger = Int(theError) {
+if let theError = errorCodeString, errorCodeInteger = Int(theError) where errorCodeInteger == 404 {
     print("\(theError): \(errorCodeInteger)")
 }
