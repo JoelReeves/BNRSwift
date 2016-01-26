dccd7ad0dd3c559387566ba75cc4416c750ac744
Joel Reeves <jreeves@tonicdesign.com>
2016-01-26 16:15:12 -0500

    determining if array is empty

--- Chapter_09/Arrays.playground/Contents.swift
+++ Chapter_09/Arrays.playground/Contents.swift
@@ -1,50 +1,8 @@
 import Cocoa
 
-var bucketList = ["Climb Mt. Everest"]
+//Bronze Challenge
 
-var newItems = ["Fly hot air balloon to Fiji",
-                "Lord of the Rings is the best movie",
-                "Go outside",
-                "Go see the new Star Wars movie"]
+var toDoList = ["Take out garbage", "Pay bills", "cross off finished items"]
 
-bucketList += newItems
-
-bucketList.removeAtIndex(4)
-
-print("There are \(bucketList.count) items in the array")
-print("bucketList capacity \(bucketList.capacity)")
-print(bucketList[0...2])
-
-bucketList[2] += " of all time!!!"
-bucketList[0] = "Climb Mt. Kilimanjaro"
-bucketList.insert("Go to Hawaii", atIndex: 2)
-
-bucketList
-
-// not equal because order of items and text is different
-var arrayNotEqual = ["Go outside!",
-                        "Fly hot air balloon to Fiji",
-                        "Go to Hawaii",
-                        "Lord of the Rings is the best movie of all time!",
-                        "Climb Mt. Kilimanjaro"]
-
-var arrayEqual = ["Climb Mt. Kilimanjaro",
-                    "Fly hot air balloon to Fiji",
-                    "Go to Hawaii",
-                    "Lord of the Rings is the best movie of all time!!!",
-                    "Go outside"]
-
-let notEqual = (bucketList == arrayNotEqual)
-let equal = (bucketList == arrayEqual)
-
-let lunches = ["Pizza",
-                "Chinese food",
-                "Sushi"]
-
-var arrayInts = [Int](count: 10, repeatedValue: 0)
-
-for var i = 1; i < 11; i++ {
-    arrayInts.append(i)
-}
-
+print("Is toDoList empty? \(toDoList.isEmpty)")
 
