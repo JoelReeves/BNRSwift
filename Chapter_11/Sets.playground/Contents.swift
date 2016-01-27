48bb2edb637ca74599758a6a241593b2896b989d
Joel Reeves <jreeves@tonicdesign.com>
2016-01-27 10:52:32 -0500

    using isSuperSetOf method to determine if one set contains all values specified in the other set

--- Chapter_11/Sets.playground/Contents.swift
+++ Chapter_11/Sets.playground/Contents.swift
@@ -1,25 +1,8 @@
 import Cocoa
 
-var groceryBag: Set = ["Apples", "Oranges", "Pears"]
+// Bronze Challenge
 
-for food in groceryBag {
-    print(food)
-}
+let myCities: Set = ["Atlanta", "Chicago", "Jacksonville", "New York", "San Francisco"]
+let yourCities: Set = ["Chicago", "San Francisco", "Jacksonville"]
 
-print("groceryBag contains \(groceryBag.count) items")
-
-groceryBag.insert("Watermelon")
-
-groceryBag.sort()
-
-let hasBananas = groceryBag.contains("Bananas")
-
-let friendsGroceryBag: Set = ["Bananas", "Cereal", "Milk", "Oranges"]
-let commonGroceryBag = groceryBag.union(friendsGroceryBag)
-
-let roommatesGroceryBag: Set = ["Apples", "Bananas", "Cereal", "Toothpaste"]
-let itemsToReturn = commonGroceryBag.intersect(roommatesGroceryBag)
-
-let yourSecondBag: Set = ["Berries", "Yogurt"]
-let roommatesSecondBag: Set = ["Grapes", "Honey"]
-let disjoint = yourSecondBag.isDisjointWith(roommatesSecondBag)
+let commonCities = myCities.isSupersetOf(yourCities)
