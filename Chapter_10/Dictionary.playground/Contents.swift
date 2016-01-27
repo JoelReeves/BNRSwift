47096bbda62f6879aba46066c1a4bbfe5ec15d9b
Joel Reeves <jreeves@tonicdesign.com>
2016-01-27 10:08:08 -0500

    placing array inside a dictionary and printing out it's values

--- Chapter_10/Dictionary.playground/Contents.swift
+++ Chapter_10/Dictionary.playground/Contents.swift
@@ -1,36 +1,10 @@
 import Cocoa
 
-var movieRatings = ["Fantastic Four": 2, "Lord of the Rings": 5, "The Dark Knight": 5]
+// Silver Challenge
 
-print("I have rated \(movieRatings.count) movies")
+var myState = ["1st County":[10000, 10001, 10002, 10003, 10004],
+                "2nd County":[11000, 11001, 11002, 11003, 11004],
+                "3rd County":[12000, 12001, 12002, 12003, 12004]]
 
-let fantasticFourRating = movieRatings["Fantastic Four"]
 
-movieRatings["Lord of the Rings"] = 3
-movieRatings
-
-let oldRating: Int? = movieRatings.updateValue(5, forKey: "Lord of the Rings")
-if let lastRating = oldRating, currentRating = movieRatings["Lord of the Rings"] {
-    print("Old rating: \(lastRating); current rating: \(currentRating)")
-}
-
-movieRatings["The Incredibles"] = 5
-
-movieRatings.removeValueForKey("Fantastic Four") // returns the value removed
-//movieRatings["Fantastic Four"] = nil   setting a key to nil can also remove from a dictionary
-movieRatings
-
-for (key, value) in movieRatings {
-    print("The movie \(key) was rated \(value)")
-}
-
-for movie in movieRatings.keys {
-    print("I have rated \(movie)")
-}
-
-for rating in movieRatings.values {
-    print("I have the ratings \(rating)")
-}
-
-let watchedMovies = Array(movieRatings.keys)
-let ratings = Array(movieRatings.values)
+print("myState has the following zip codes: \(Array(myState.values.flatten()))")
