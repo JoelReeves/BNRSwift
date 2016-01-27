9e4386fa0b21cffc090c9f4c1395cfe975f3f241
Joel Reeves <jreeves@tonicdesign.com>
2016-01-27 09:29:17 -0500

    initializing, adding and removing values

---
+++ Chapter_10/Dictionary.playground/Contents.swift
@@ -0,0 +1,22 @@
+import Cocoa
+
+var movieRatings = ["Fantastic Four": 2, "Lord of the Rings": 5, "The Dark Knight": 5]
+
+print("I have rated \(movieRatings.count) movies")
+
+let fantasticFourRating = movieRatings["Fantastic Four"]
+
+movieRatings["Lord of the Rings"] = 3
+movieRatings
+
+let oldRating: Int? = movieRatings.updateValue(5, forKey: "Lord of the Rings")
+if let lastRating = oldRating, currentRating = movieRatings["Lord of the Rings"] {
+    print("Old rating: \(lastRating); current rating: \(currentRating)")
+}
+
+movieRatings["The Incredibles"] = 5
+
+movieRatings.removeValueForKey("Fantastic Four") // returns the value removed
+//movieRatings["Fantastic Four"] = nil   setting a key to nil can also remove from a dictionary
+movieRatings
+
