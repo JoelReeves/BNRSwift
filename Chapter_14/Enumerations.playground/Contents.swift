45c2f60c1dc09ae8da7edf50473c7e6cf39b2a95
Joel Reeves <jreeves@tonicdesign.com>
2016-01-28 14:14:08 -0500

    associated values

--- Chapter_14/Enumerations.playground/Contents.swift
+++ Chapter_14/Enumerations.playground/Contents.swift
@@ -82,3 +82,30 @@ print("the bulb's temperature is \(bulbTemperature)")
 bulb.toggle()
 bulbTemperature = bulb.surfaceTemperatureForAmbientTemperature(ambientTemperature)
 print("the bulb's temperature is \(bulbTemperature)")
+
+
+// associated values
+enum ShapeDimensions {
+    case Point
+    case Square(Double)
+    case Rectangle(width: Double, height: Double)
+    
+    func area() -> Double {
+        switch self {
+        case .Point:
+            return 0
+        case let .Square(side):
+            return side * side
+        case let .Rectangle(width: w, height: h):
+            return w * h
+        }
+    }
+}
+
+var squareShape = ShapeDimensions.Square(10.0)
+var rectShape = ShapeDimensions.Rectangle(width: 5.0, height: 10.0)
+var pointShape = ShapeDimensions.Point
+
+print("square's area = \(squareShape.area())")
+print("rectangle's area = \(rectShape.area())")
+print("point's area = \(pointShape.area())")
