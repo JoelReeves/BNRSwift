8e215a8f7bd14e0d7bf49ee00aa37d35c5d86b9e
Joel Reeves <jreeves@tonicdesign.com>
2016-01-28 13:25:09 -0500

    Raw Value Enumerations

--- Chapter_14/Enumerations.playground/Contents.swift
+++ Chapter_14/Enumerations.playground/Contents.swift
@@ -1,13 +1,19 @@
 import Cocoa
 
-enum TextAlignment {
-    case Left
-    case Right
-    case Center
-    case Justify
+enum TextAlignment: Int {
+    case Left = 20 // specifying raw values
+    case Right = 30
+    case Center = 40
+    case Justify = 50
 }
 
 var alignment = TextAlignment.Justify
+
+print("Left has raw value \(TextAlignment.Left.rawValue)")
+print("Right has raw value \(TextAlignment.Right.rawValue)")
+print("Center has raw value \(TextAlignment.Center.rawValue)")
+print("Justify has raw value \(TextAlignment.Justify.rawValue)")
+print("The alignment variable has raw value \(alignment.rawValue)")
 
 switch alignment {
 case .Left:
@@ -19,3 +25,25 @@ case .Center:
 case .Justify:
     print("justified")
 }
+
+// converting raw values to enums
+let myRawValue = 20
+
+if let myAlignment = TextAlignment(rawValue: myRawValue) {
+    print("successfully converted \(myRawValue) into a TextAlignment")
+} else {
+    print("\(myRawValue) has no corresponding TextAlignment case")
+}
+
+
+// creating enum with strings
+enum ProgrammingLanguage: String {
+    case Swift // if you don't specify a value, Swift uses the case name for the value
+    case ObjC = "Objective-C"
+    case Java
+    case C
+    case Kotlin
+}
+
+let myFavoriteLanguage = ProgrammingLanguage.Java
+print("My favorite programming language is \(myFavoriteLanguage.rawValue)")
