e526b9425783908a946d32c9a79a2c3bba13d037
Joel Reeves <jreeves@tonicdesign.com>
2016-01-29 09:45:41 -0500

    creating a struct and initializing it

---
+++ Chapter_15/MonsterTown/MonsterTown/Town.swift
@@ -0,0 +1,14 @@
+//
+//  Town.swift
+//  MonsterTown
+//
+//  Created by Joel Reeves on 1/29/16.
+//  Copyright © 2016 Bromance Labs. All rights reserved.
+//
+
+import Foundation
+
+struct Town {
+    var population = 5422
+    var numberOfStoplights = 4
+}
---
+++ Chapter_15/MonsterTown/MonsterTown/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  MonsterTown
+//
+//  Created by Joel Reeves on 1/28/16.
+//  Copyright © 2016 Bromance Labs. All rights reserved.
+//
+
+import Foundation
+
+var myTown = Town()
+
+print("Population: \(myTown.population), number of stoplights: \(myTown.numberOfStoplights)")
+
