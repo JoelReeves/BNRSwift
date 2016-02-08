79c902a1f19afbc8f640c320b8ed779ad582245d
Joel Reeves <jreeves@tonicdesign.com>
2016-02-08 09:16:42 -0500

    using a computed property

--- Chapter_16/MonsterTown/MonsterTown/Town.swift
+++ Chapter_16/MonsterTown/MonsterTown/Town.swift
@@ -19,18 +19,20 @@ struct Town {
         case Large
     }
     
-    lazy var townSize: Size = {
-        switch self.population {
-        case 0...10000:
-            return Size.Small
-            
-        case 10001...100000:
-            return Size.Medium
-            
-        default:
-            return Size.Large
+    var townSize: Size {
+        get {
+            switch self.population {
+            case 0...10000:
+                return Size.Small
+                
+            case 10001...100000:
+                return Size.Medium
+                
+            default:
+                return Size.Large
+            }
         }
-    }()
+    }
     
     func printTownDescription() {
         print("Population: \(population); number of stoplights: \(numberOfStoplights)")
