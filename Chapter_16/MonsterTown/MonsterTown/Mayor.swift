919f4f8e0d79a48b63b982a159cac1831e20968d
Joel Reeves <jreeves@tonicdesign.com>
2016-02-09 14:29:34 -0500

    using private instance property; increasing this property each time a Mayor instance is notified of a Zombie attack and the population decreases

--- Chapter_16/MonsterTown/MonsterTown/Mayor.swift
+++ Chapter_16/MonsterTown/MonsterTown/Mayor.swift
@@ -9,8 +9,11 @@
 import Foundation
 
 struct Mayor {
+    private var anxietyLevel = 0
     
-    func decreasePopulationSpeech() {
+    mutating func decreasePopulationSpeech() {
         print("I'm deeply saddened to hear about this latest tragedy. I promise that my office is looking into the nature of this rash of violence")
+        anxietyLevel += 1
+        print("Current axienty level: \(anxietyLevel)")
     }
 }
