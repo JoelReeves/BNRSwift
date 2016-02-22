8dc298ce0e25efb37f213cc522fda02e6f397b79
Joel Reeves <jreeves@tonicdesign.com>
2016-02-22 10:16:13 -0500

    value semantics

---
+++ Chapter_18/ValuesVsRef.playground/Contents.swift
@@ -0,0 +1,8 @@
+import Cocoa
+
+var str = "Hello, playground"
+
+var playgroundGreeting = str // giving copy of str's value to playgroundGreeting
+playgroundGreeting += "! How are you today?"
+str
+
