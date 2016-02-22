1fb38d6db78d473194e857129f02d72bf5c9342d
Joel Reeves <jreeves@tonicdesign.com>
2016-02-22 10:22:30 -0500

    reference semantics

--- Chapter_18/ValuesVsRef.playground/Contents.swift
+++ Chapter_18/ValuesVsRef.playground/Contents.swift
@@ -6,3 +6,22 @@ var playgroundGreeting = str // giving copy of str's value to playgroundGreeting
 playgroundGreeting += "! How are you today?"
 str
 
+
+class GreekGod {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+let hecate = GreekGod(name: "Hecate")
+let anotherHecate = hecate
+
+// assigning a class to a variable/constant causes the variable/constant to get a reference to the instance
+// changing one will change the other
+
+anotherHecate.name = "AnotherHecate"
+anotherHecate.name
+hecate.name
+
