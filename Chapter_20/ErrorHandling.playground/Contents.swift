55fd59e02f684b1e69c90879edd4807bb894479a
Joel Reeves <jreeves@tonicdesign.com>
2016-02-23 09:48:16 -0500

    creating lex method

--- Chapter_20/ErrorHandling.playground/Contents.swift
+++ Chapter_20/ErrorHandling.playground/Contents.swift
@@ -25,4 +25,30 @@ class Lexer {
         assert(position < input.endIndex, "Cannot advance past the end!")
         position.advancedBy(1)
     }
+    
+    func lex() throws -> [Token] {
+        var tokens = [Token]()
+        
+        while let nextCharacter = peek() {
+            switch nextCharacter {
+            case "0" ... "9":
+                // start of a number - need to grab the rest
+                break
+                
+            case "+":
+                tokens.append(.Plus)
+                advance()
+            
+            case " ":
+                // just advance to ignore spaces
+                advance()
+                
+            default:
+                // something unexpected - need to send back an error
+                break
+            }
+        }
+        
+        return tokens
+    }
 }
