4c75fe8b73e17fe950d647a614d19a2aab1620c9
Joel Reeves <jreeves@tonicdesign.com>
2016-02-26 16:45:26 -0500

    extending Velocity; adding 2 computed types

--- Chapter_21/Extensions.playground/Contents.swift
+++ Chapter_21/Extensions.playground/Contents.swift
@@ -2,4 +2,12 @@ import Cocoa
 
 typealias Velocity = Double
 
-
+extension Velocity {
+    var kph: Velocity {
+        return self * 1.60934
+    }
+    
+    var mph: Velocity {
+        return self
+    }
+}
