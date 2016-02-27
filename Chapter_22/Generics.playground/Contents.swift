7d2460aeab4259c46260b72846bb15e199f7ecc8
Joel Reeves <jreeves@tonicdesign.com>
2016-02-27 12:10:56 -0500

    using a type constraint to check CustomStringConvertible valued

--- Chapter_22/Generics.playground/Contents.swift
+++ Chapter_22/Generics.playground/Contents.swift
@@ -65,3 +65,12 @@ func checkIfEqual<T: Equatable>(first: T, _ second: T) -> Bool {
 print(checkIfEqual(1, 1))
 print(checkIfEqual("a string", "a string"))
 print(checkIfEqual("a string", "a different string"))
+
+
+func checkIfDescriptionsMatch<T: CustomStringConvertible, U: CustomStringConvertible>(first: T, _ second: U) -> Bool {
+    return first.description == second.description
+}
+
+print(checkIfDescriptionsMatch(Int(1), UInt(1)))
+print(checkIfDescriptionsMatch(1, 1.0))
+print(checkIfDescriptionsMatch(Float(1.0), Double(1.0)))
