904e16dc2d5b469f82910b96915b9819b43a784c
Joel Reeves <jreeves@tonicdesign.com>
2016-02-27 11:23:38 -0500

    extending Int to have a timesFive property

--- Chapter_21/Extensions.playground/Contents.swift
+++ Chapter_21/Extensions.playground/Contents.swift
@@ -1,87 +1,11 @@
 import Cocoa
 
-typealias Velocity = Double
+// Bronze challenge
 
-extension Velocity {
-    var kph: Velocity {
-        return self * 1.60934
-    }
-    
-    var mph: Velocity {
-        return self
+extension Int {
+    var timesFive: Int {
+        return self * 5
     }
 }
 
-protocol VehicleType {
-    var topSpeed: Velocity { get }
-    var numberOfDoors: Int { get }
-    var hasFlatbed: Bool { get }
-}
-
-struct Car {
-    let make: String
-    let model: String
-    let year: Int
-    let color: String
-    let nickname: String
-    var gasLevel: Double {
-        willSet {
-            precondition(newValue <= 1.0 && newValue >= 0.0, "New value must be between 0 and 1.")
-        }
-    }
-}
-
-extension Car: VehicleType {
-    var topSpeed: Velocity { return 180 }
-    var numberOfDoors: Int { return 4 }
-    var hasFlatbed: Bool { return false }
-}
-
-extension Car {
-    init(carMake: String, carModel: String, carYear: Int) {
-        self.init(make: carMake, model: carModel, year: carYear, color: "Black", nickname: "N/A", gasLevel: 1.0)
-    }
-}
-
-var car = Car(carMake: "Toyota", carModel: "Prius", carYear: 2016)
-
-
-extension Car {
-    enum CarKind: CustomStringConvertible {
-        case Coupe, Sedan
-        var description: String {
-            switch self {
-            case .Coupe:
-                return "Coupe"
-            case .Sedan:
-                return "Sedan"
-            }
-        }
-    }
-    var kind: CarKind {
-        if numberOfDoors == 2 {
-            return .Coupe
-        } else {
-            return .Sedan
-        }
-    }
-}
-
-car.kind.description
-
-
-extension Car {
-    mutating func emptyGas(amount: Double) {
-        precondition(amount <= 1 && amount > 0, "Amount to remove must be between 0 and 1")
-        gasLevel -= amount
-    }
-    
-    mutating func fillGas() {
-        gasLevel = 1.0
-    }
-}
-
-car.emptyGas(0.3)
-car.gasLevel
-car.fillGas()
-car.gasLevel
+5.timesFive
