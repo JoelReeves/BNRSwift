9f6a117168402403bcfc7f2935aa146000158292
Joel Reeves <jreeves@tonicdesign.com>
2016-02-27 10:42:59 -0500

    creating a Struct for cars

--- Chapter_21/Extensions.playground/Contents.swift
+++ Chapter_21/Extensions.playground/Contents.swift
@@ -17,3 +17,16 @@ protocol VehicleType {
     var numberOfDoors: Int { get }
     var hasFlatbed: Bool { get }
 }
+
+struct Car {
+    let make: String
+    let model: String
+    let year: Int
+    let color: String
+    let nickname: String
+    var gasLevel: Double {
+        willSet {
+            precondition(newValue <= 1.0 && newValue >= 0.0, "New value must be between 0 and 1.")
+        }
+    }
+}
