9a72d841b7ce77dbf7d25d239d680a0d77946b01
Joel Reeves <jreeves@tonicdesign.com>
2016-03-08 15:16:57 -0500

    overloading + operator to add Points together

--- Chapter_25/Comparison.playground/Contents.swift
+++ Chapter_25/Comparison.playground/Contents.swift
@@ -13,6 +13,10 @@ func <(lhs: Point, rhs: Point) -> Bool {
     return (lhs.x < rhs.x) && (lhs.x < rhs.y)
 }
 
+func +(lhs: Point, rhs: Point) -> Point {
+    return Point(x: (lhs.x + rhs.x), y: (lhs.y + rhs.y))
+}
+
 let a = Point(x: 3, y: 4)
 let b = Point(x: 3, y: 4)
 
@@ -28,3 +32,9 @@ let cLessThanD = (c < d)
 let cLessTahnEqualD = (c <= d)
 let cGreaterThanD = (c > d)
 let cGreaterThanEqualD = (c >= d)
+
+let e = Point(x: 2, y: 2)
+let f = Point(x: 3, y: 4)
+let addedPoint = e + f
+print(addedPoint)
+
