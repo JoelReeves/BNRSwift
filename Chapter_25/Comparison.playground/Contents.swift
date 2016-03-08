21430251783e94fa1d9a45a512399700b59b59a8
Joel Reeves <jreeves@tonicdesign.com>
2016-03-08 14:46:52 -0500

    making Point conform to Equatable protocol

--- Chapter_25/Comparison.playground/Contents.swift
+++ Chapter_25/Comparison.playground/Contents.swift
@@ -1,11 +1,15 @@
 import Cocoa
 
-struct Point {
+struct Point: Equatable {
     let x: Int
     let y: Int
+}
+
+func ==(lhs: Point, rhs: Point) -> Bool {
+    return (lhs.x == rhs.x) && (lhs.y == rhs.y)
 }
 
 let a = Point(x: 3, y: 4)
 let b = Point(x: 3, y: 4)
 
-let abEqual = (a == b) // generates error because Point struct doesn't know how to test for equality
+let abEqual = (a == b) // now works since we definined the == operator for Points
