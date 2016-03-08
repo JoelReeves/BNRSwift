59e51a180c26c704f0d665971db7769a6b1efa38
Joel Reeves <jreeves@tonicdesign.com>
2016-03-08 14:58:45 -0500

    conforming to Comparable protocol

--- Chapter_25/Comparison.playground/Contents.swift
+++ Chapter_25/Comparison.playground/Contents.swift
@@ -1,6 +1,6 @@
 import Cocoa
 
-struct Point: Equatable {
+struct Point: Equatable, Comparable {
     let x: Int
     let y: Int
 }
@@ -9,8 +9,22 @@ func ==(lhs: Point, rhs: Point) -> Bool {
     return (lhs.x == rhs.x) && (lhs.y == rhs.y)
 }
 
+func <(lhs: Point, rhs: Point) -> Bool {
+    return (lhs.x < rhs.x) && (lhs.x < rhs.y)
+}
+
 let a = Point(x: 3, y: 4)
 let b = Point(x: 3, y: 4)
 
 let abEqual = (a == b) // now works since we definined the == operator for Points
 let abNotEqual = (a != b) // two Points are equal i.e not unequal
+
+let c = Point(x: 2, y: 6)
+let d = Point(x: 3, y: 7)
+
+let cdEqual = (c == d)
+let cLessThanD = (c < d)
+
+let cLessTahnEqualD = (c <= d)
+let cGreaterThanD = (c > d)
+let cGreaterThanEqualD = (c >= d)
