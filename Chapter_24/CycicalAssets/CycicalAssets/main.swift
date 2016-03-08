ac6c5186774f9c6b046610c1c290ca4f1d8fdf90
Joel Reeves <jreeves@tonicdesign.com>
2016-03-08 14:14:09 -0500

    adding an Accountant to the Person class

--- Chapter_24/CycicalAssets/CycicalAssets/main.swift
+++ Chapter_24/CycicalAssets/CycicalAssets/main.swift
@@ -10,6 +10,7 @@ import Foundation
 
 class Person: CustomStringConvertible {
     let name: String
+    let accountant = Accountant()
     var assets = [Asset]()
     
     var description: String {
@@ -18,6 +19,13 @@ class Person: CustomStringConvertible {
     
     init(name: String) {
         self.name = name
+        
+        accountant.netWorthChangedhandler = {
+            netWorth in
+            
+            self.netWorthDidChange(netWorth)
+            return
+        }
     }
     
     deinit {
@@ -27,6 +35,11 @@ class Person: CustomStringConvertible {
     func takeOwnershipOfAsset(asset: Asset) {
         asset.owner = self
         assets.append(asset)
+        accountant.gainedNewAsset(asset)
+    }
+    
+    func netWorthDidChange(netWorth: Double) {
+        print("The net worth of \(self) is now \(netWorth)")
     }
 }
 
