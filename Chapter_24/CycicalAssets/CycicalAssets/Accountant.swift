e3e8bdac2107c4d48c3d02f1923745142bd8867b
Joel Reeves <jreeves@tonicdesign.com>
2016-03-08 14:10:17 -0500

    defining an Accountant

---
+++ Chapter_24/CycicalAssets/CycicalAssets/Accountant.swift
@@ -0,0 +1,24 @@
+//
+//  Accountant.swift
+//  CycicalAssets
+//
+//  Created by Joel Reeves on 3/8/16.
+//  Copyright © 2016 Bromance Labs. All rights reserved.
+//
+
+import Foundation
+
+class Accountant {
+    typealias NetWorthChanged = (Double) -> ()
+    
+    var netWorthChangedhandler: NetWorthChanged? = nil
+    var netWorth: Double = 0.0 {
+        didSet {
+            netWorthChangedhandler?(netWorth)
+        }
+    }
+    
+    func gainedNewAsset(asset: Asset) {
+        netWorth += asset.value
+    }
+}
