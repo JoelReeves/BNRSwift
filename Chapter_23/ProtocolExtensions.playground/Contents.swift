8844ac9c441ed82f7ba2195cfd6580049cd3b36a
Joel Reeves <jreeves@tonicdesign.com>
2016-03-08 11:05:06 -0500

    defining structs that conform to the ExerciseType protocol

--- Chapter_23/ProtocolExtensions.playground/Contents.swift
+++ Chapter_23/ProtocolExtensions.playground/Contents.swift
@@ -5,3 +5,16 @@ protocol ExerciseType {
     var caloriesBurned: Double { get }
     var minutes: Double { get }
 }
+
+struct EllipticalTrainer: ExerciseType {
+    let name = "Elliptical Machine"
+    let caloriesBurned: Double
+    let minutes: Double
+}
+
+struct Treadmill: ExerciseType {
+    let name = "Treadmill"
+    let caloriesBurned: Double
+    let minutes: Double
+    let distanceInMiles: Double
+}
