fb8d1ad82652761ab5af9c83e9495b6f215dd327
Joel Reeves <jreeves@tonicdesign.com>
2016-03-09 10:14:19 -0500

    implementing dataOfType to save documents

--- Chapter_26/VocalTextEdit/VocalTextEdit/Document.swift
+++ Chapter_26/VocalTextEdit/VocalTextEdit/Document.swift
@@ -24,6 +24,15 @@ class Document: NSDocument {
     override func dataOfType(typeName: String) throws -> NSData {
         let windowController = windowControllers[0]
         let viewController = windowController.contentViewController as! ViewController
+        
+        let contents = viewController.textView.string ?? ""
+        
+        if let data = contents.dataUsingEncoding(NSUTF8StringEncoding) {
+            return data
+        } else {
+            let userInfo = [NSLocalizedRecoverySuggestionErrorKey:"File cannot be encoded in UTF-8"]
+            throw NSError(domain: "com.bromancelabs.VocalTextEdit", code: 0, userInfo: userInfo)
+        }
     }
 
     override func readFromData(data: NSData, ofType typeName: String) throws {
