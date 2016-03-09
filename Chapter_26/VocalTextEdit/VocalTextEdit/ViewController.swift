68e4c56a1e071b330858be3ed749f19835044a6a
Joel Reeves <jreeves@tonicdesign.com>
2016-03-09 13:52:38 -0500

    updating progress bar when speaking

--- Chapter_26/VocalTextEdit/VocalTextEdit/ViewController.swift
+++ Chapter_26/VocalTextEdit/VocalTextEdit/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: NSViewController, NSSpeechSynthesizerDelegate {
     
     let speechSynthesizer = NSSpeechSynthesizer()
     
+    var delta: Double = 0.0
+    var length: Int = 0
+    var duration: Int = 0
+    
     @IBOutlet weak var progressBar: NSProgressIndicator!
     @IBOutlet weak var speakButton: NSButton!
     @IBOutlet weak var stopButton: NSButton!
@@ -37,6 +41,12 @@ class ViewController: NSViewController, NSSpeechSynthesizerDelegate {
     func speechSynthesizer(sender: NSSpeechSynthesizer, didFinishSpeaking finishedSpeaking: Bool) {
         speakButton.enabled = true
         stopButton.enabled = false
+        progressBar.incrementBy(-100.0)
+        progressBar.displayedWhenStopped = false
+    }
+    
+    func speechSynthesizer(sender: NSSpeechSynthesizer, willSpeakWord characterRange: NSRange, ofString string: String) {
+        progressBar.incrementBy(self.delta)
     }
     
     @IBAction func speakButtonClicked(sender: NSButton) {
@@ -44,10 +54,22 @@ class ViewController: NSViewController, NSSpeechSynthesizerDelegate {
         stopButton.enabled = true
         
         if let contents = textView.string where !contents.isEmpty {
+            self.length = contents.characters.count
+            self.duration = length / 6
+            self.delta = Double(100 / self.duration)
+            
+            progressBar.minValue = 0.0
+            progressBar.maxValue = 100.0
+            
             speechSynthesizer.startSpeakingString(contents)
         } else {
+            self.duration = 4
+            self.delta = 25.0
+            
             speechSynthesizer.startSpeakingString("The document is empty")
         }
+        
+        print("Length: \(self.length), Duration: \(self.duration), Delta: \(self.delta)")
     }
     
     @IBAction func stopButtonClicked(sender: NSButton) {
