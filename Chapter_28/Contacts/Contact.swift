0caba8369bd46276e92f073c4bb6202c0e597d9d
Joel Reeves <jreeves@tonicdesign.com>
2016-03-10 09:40:06 -0500

    creating a Contact class

---
+++ Chapter_28/Contacts/Contact.swift
@@ -0,0 +1,18 @@
+//
+//  Contact.swift
+//  Contacts
+//
+//  Created by Joel Reeves on 3/10/16.
+//  Copyright © 2016 Bromance Labs. All rights reserved.
+//
+
+import Foundation
+
+
+class Contact: NSObject {
+    let name: String
+    
+    init(contactName: String) {
+        name = contactName
+    }
+}
