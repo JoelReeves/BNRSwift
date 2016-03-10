b252539aca599dcfef151999a6620c821914b94a
Joel Reeves <jreeves@tonicdesign.com>
2016-03-10 11:25:32 -0500

    setting the UIImageView's image via the DefaultImage's generateDefaultImageOfSize method

--- Chapter_28/Contacts/Contacts/NewContactViewController.swift
+++ Chapter_28/Contacts/Contacts/NewContactViewController.swift
@@ -17,7 +17,7 @@ class NewContactViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        contactImageView.image = DefaultImage.generateDefaultImageOfSize(contactImageView.frame.size)
     }
 
     override func didReceiveMemoryWarning() {
