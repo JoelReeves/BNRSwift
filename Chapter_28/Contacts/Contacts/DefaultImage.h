7d1a35b5de21c166ec93f7d4e1adad2bc57a24f3
Joel Reeves <jreeves@tonicdesign.com>
2016-03-10 11:00:23 -0500

    Creating class to load default images Added ImageView with constraints to storyboard

---
+++ Chapter_28/Contacts/Contacts/DefaultImage.h
@@ -0,0 +1,13 @@
+//
+//  DefaultImage.h
+//  Contacts
+//
+//  Created by Joel Reeves on 3/10/16.
+//  Copyright © 2016 Bromance Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DefaultImage : NSObject
+
+@end
---
+++ Chapter_28/Contacts/Contacts/DefaultImage.m
@@ -0,0 +1,13 @@
+//
+//  DefaultImage.m
+//  Contacts
+//
+//  Created by Joel Reeves on 3/10/16.
+//  Copyright © 2016 Bromance Labs. All rights reserved.
+//
+
+#import "DefaultImage.h"
+
+@implementation DefaultImage
+
+@end
